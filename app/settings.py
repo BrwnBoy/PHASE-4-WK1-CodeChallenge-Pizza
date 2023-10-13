e719cfdb464286cd3fd6e222e1f5336afb9e622d
Brwni <brianmm665@gmial.com>
2023-10-13 21:33:13 +0300

    Update

--- app/settings.py
+++ app/settings.py
@@ -5,5 +5,5 @@ import os
 load_dotenv()
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://scott:tiger@localhost/restaurant_pizza.db")
+SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://scott:tiger@localhost/restaurant_pizza.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
